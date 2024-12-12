8e0586e79778b17879185e3a6763e6316bbf1e3c
Carlson29 <kometacarlson@gmail.com>
2024-12-12 14:33:23 +0000

    making put song along with error messages

---
+++ MusicBlazor/Artists_Songs.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MusicBlazor
+{
+	public class Artists_Songs
+	{
+		public int Id { get; set; }
+		
+		public int Song_Id { get; set; }
+		
+		public int Artist_Id { get; set; }
+	}
+}
--- MusicBlazor/Songs.cs
+++ MusicBlazor/Songs.cs
@@ -9,6 +9,6 @@ public class Class1
 	public int id { get; set; }
 	public string title { get; set; }
 	public string genre { get; set; }
-	public string duration { get; set; }
+	public TimeSpan duration { get; set; }
 	public DateTime releaseDate { get; set; }
 }
